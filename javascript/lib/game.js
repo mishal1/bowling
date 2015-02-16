b69d12b3eba361c65ec6e8c6fd3139485bf4839e
Mishal Islam <mishal.islam01@gmail.com>
2015-02-16 08:52:13 +0000

    restarted

--- javascript/lib/game.js
+++ javascript/lib/game.js
@@ -1,17 +1,14 @@
 function Game(){
-	this.total = 0
 	this.frameCount = 1
+  this.allFrames = []
+};
+
+Game.prototype.totalScore = function() {
+  var total = 0
+  for(var i = 0; i < this.allFrames.length; i++){total += this.allFrames[i]}
+  return total
 };
 
 Game.prototype.roll = function(number) {
-	if(this.validateNumber(number)){ return this.addScore(number) }
-	return 'Give me a whole number which is between 1 and 10'
-};
-
-Game.prototype.addScore = function(number) {
-	this.total += number
-};
-
-Game.prototype.validateNumber = function(number) {
-	return number <=10 && number % 1 === 0 && number >= 0
+  this.allFrames.push(number)
 };
--- javascript/spec/gameSpec.js
+++ javascript/spec/gameSpec.js
@@ -7,34 +7,12 @@ describe('Bowling', function(){
 	});
 
 	it('has a total of 0 at the beginning of the game', function(){
-		expect(bowling.total).toEqual(0)
+		expect(bowling.totalScore()).toEqual(0)
 	});
 
-	it('the total increases after a user rolls', function(){
+	it('the total score increases when a user rolls', function(){
 		bowling.roll(1)
-		expect(bowling.total).toEqual(1)
-	});
-
-	it('the roll value cannot be greater than 10', function(){
-		expect(bowling.roll(11)).toEqual("Give me a whole number which is between 1 and 10")
-	});
-
-	it('the roll must be an integer', function(){
-		expect(bowling.roll(0.5)).toEqual("Give me a whole number which is between 1 and 10")
-	});
-
-	it('the roll must be greater than 0', function(){
-		expect(bowling.roll(-1)).toEqual("Give me a whole number which is between 1 and 10")
-	});
-
-	it('the game starts from the first frame', function(){
-		expect(bowling.frameCount).toEqual(1)
-	});
-
-	it('each frame has two rolls', function(){
-		bowling.roll(1)
-		bowling.roll(1)
-		expect(bowling.frameCount).toEqual(2)
+		expect(bowling.totalScore()).toEqual(1)
 	});
 
 });
