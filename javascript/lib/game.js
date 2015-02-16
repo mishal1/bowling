f88a233a75be74ac9cd0256f1227c88d7344b455
Mishal Islam <mishal.islam01@gmail.com>
2015-02-16 13:51:46 +0000

    more refactoring

--- javascript/lib/game.js
+++ javascript/lib/game.js
@@ -5,7 +5,7 @@ function Game(){
 
 Game.prototype.totalScore = function() {
   var total = 0
-  for(var i = 0; i < this.allFrames.length; i++){total += this.allFrames[i]}
+  for(var i = 0; i < this.frameLength(); i++){total += this.allFrames[i]}
   return total + this.bonus
 };
 
@@ -20,13 +20,13 @@ Game.prototype.validateNumber = function(number) {
 };
 
 Game.prototype.validValue = function(number) {
-  var length = this.allFrames.length
-  if(length % 2 != 0){return this.checkSumOfFrame(length, number)}
+  var length = this.frameLength()
+  if(this.frameLength() % 2 != 0){return this.checkSumOfFrame(number)}
   return true
 };
 
-Game.prototype.checkSumOfFrame = function(length, number) {
-  var sumOfFrame = this.allFrames[length-1] + number
+Game.prototype.checkSumOfFrame = function(number) {
+  var sumOfFrame = this.allFrames[this.frameLength()-1] + number
   if(sumOfFrame > 10){return false}
   return true
 };
@@ -37,38 +37,31 @@ Game.prototype.addScore = function(number) {
 };
 
 Game.prototype.checkBonus = function() {
-  var length = this.allFrames.length
-  if(length % 2 === 0){
-    this.checkIfStrikeOrSpare(length)
-  }
+  var length = this.frameLength()
+  if(this.frameLength() % 2 === 0){this.checkIfStrikeOrSpare()}
 };
 
-Game.prototype.checkIfStrikeOrSpare = function(length) {
-  this.allFrames[length - 4] === 10 ? this.addStrikeBonus(length) : this.spareBonus(length)
+Game.prototype.checkIfStrikeOrSpare = function() {
+  this.allFrames[this.frameLength() - 4] === 10 ? this.addStrikeBonus() : this.spareBonus()
 };
 
 
 Game.prototype.addStrikeBonus = function(length) {
-  for(var i = 1; i < 3; i++){this.bonus += this.allFrames[length - i]}
+  for(var i = 1; i < 3; i++){this.bonus += this.allFrames[this.frameLength() - i]}
 };
 
 Game.prototype.spareBonus = function(length) {
-  if(this.allFrames[length-3] + this.allFrames[length-4] === 10){
-    this.bonus += this.allFrames[length-2]
-  }
+  if(this.allFrames[this.frameLength()-3] + this.allFrames[this.frameLength()-4] === 10){ this.bonus += this.allFrames[this.frameLength()-2]}
 };
 
 Game.prototype.checkGameStatus = function() {
-  if(this.allFrames.length >= 20){return false}
+  if(this.frameLength()>= 20){return false}
   return true
 };
 
 Game.prototype.checkIfGameIsOver = function(number) {
-  if(this.checkGameStatus()){
-    this.addScore(number)
-  } else { 
-    return this.checkIfStrikeOrSpareInLastFrame(number)
-  }
+  if(this.checkGameStatus()){this.addScore(number)} 
+  else {return this.checkIfStrikeOrSpareInLastFrame(number)}
 };
 
 Game.prototype.lastFrameSum = function() {
@@ -76,15 +69,16 @@ Game.prototype.lastFrameSum = function() {
 };
 
 Game.prototype.checkIfStrikeOrSpareInLastFrame = function(number) {
-  if( this.lastFrameSum() === 10 && this.allFrames.length < 22){
-    this.lastFrameBonus(number)
-  } else {
-    return "The game is over"
-  }
+  if( this.lastFrameSum() === 10 && this.frameLength() < 22){this.lastFrameBonus(number)} 
+  else {return "The game is over"}
 };
 
 Game.prototype.lastFrameBonus = function(number) {
   this.allFrames.push(number)
   if(this.allFrames[20]!= 0){this.allFrames.push(0)}
+};
+
+Game.prototype.frameLength = function() {
+  return this.allFrames.length
 };
 
